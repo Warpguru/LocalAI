832a2928e16e5084eeb6cc6d4716f2778a11ca51
Roman Stangl <Roman.Stangl@gmx.net>
2025-10-23 10:49:10 +0200

    Documentation enhancements

--- Llama.cpp/Server.Mistral7b.cmd
+++ Llama.cpp/Server.Mistral7b.cmd
@@ -1,5 +1,5 @@
 @ECHO OFF
-START "Llama.cpp Mistral 7B" .\Llama.cpp\llama-server --model ".\models/Mistral 7B Instruct v0.3\Mistral-7B-Instruct-v0.3-Q4_K_M.gguf" --host 0.0.0.0 --port 10000 --ctx-size 32768 --n-gpu-layers 40 --jinja
+START "Llama.cpp Mistral 7B" .\Llama.cpp\llama-server --model ".\models\Mistral 7B Instruct v0.3\Mistral-7B-Instruct-v0.3-Q4_K_M.gguf" --host 0.0.0.0 --port 10000 --ctx-size 32768 --n-gpu-layers 40 --jinja
 ECHO Please be patient for Llama.cpp to initialize ...
 timeout 30
 START http://127.0.0.1:10000
